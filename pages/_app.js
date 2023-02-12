1530be6c6d53ad04a310529336b0dee2413f84a5
Salmonbear <si_bruce@hotmail.com>
2023-02-12 14:55:38 +0000

    Update _app.js
    adding plausible

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import Head from "next/head";
-import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,16 +11,16 @@ function MyApp({ Component, pageProps }) {
       id="beam-analytics"
       strategy="afterInteractive"
       src="https://beamanalytics.b-cdn.net/beam.min.js"
-      data-token="f76cd8af-c1f3-4945-8694-b3733e439047"
+      data-token="e7964972-6ea2-46f7-b258-af3be2a29496"
       async>
-      </Script><Script
+    </Script><Script
       id="plausible"
-      defer data-domain="hecto.io"
-      src="https://plausible.io/js/script.js">
-      </Script></>
-
-      </Script></>
+      strategy="afterInteractive"
+      src="https://beamanalytics.b-cdn.net/beam.min.js"
+      data-token="e7964972-6ea2-46f7-b258-af3be2a29496"
+      async>
+      </Script>
+    </>
   );
 }
 export default MyApp
-  
